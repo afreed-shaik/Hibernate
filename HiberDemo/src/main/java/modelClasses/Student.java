83a08c6fff669fdd53ef00a38596f064380186f4
afreed-shaik <afreed405shaik@gmail.com>
2021-05-12 23:07:22 +0530

    commit

---
+++ HiberDemo/src/main/java/modelClasses/Student.java
@@ -0,0 +1,30 @@
+package modelClasses;
+
+public class Student {
+	private int sId;
+	private double marks;
+	private String name;
+	public Student() {
+	
+	}
+	public int getsId() {
+		return sId;
+	}
+	public void setsId(int sId) {
+		this.sId = sId;
+	}
+	public double getMarks() {
+		return marks;
+	}
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
